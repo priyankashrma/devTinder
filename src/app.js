9c6456031d0e3ecc4f3a60e81ebe2ea6be8787b3
Priyanka Sharma <priyankasharma.prisha@gmail.com>
2026-04-24 14:58:43 +0530

    GET apis

--- src/app.js
+++ src/app.js
@@ -5,18 +5,41 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user.js");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Rahul",
-    lastName: "Sharma",
-    emailId: "sharma00praful@gmail.com",
-    password: "praful@123",
-  });
+  const user = new User(req.body);
+  console.log(user);
   try {
     await user.save();
     res.send("User successfully added");
   } catch (err) {
     res.status(400).send("Error saving the User: ", err);
+  }
+});
+
+//Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length == 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Feed API = Get All users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
